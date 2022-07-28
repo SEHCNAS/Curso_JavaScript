55cd3f549e1434571c41da0633c873a8c25e25d0
Gabriel Sanches <thebug@outlook.com.br>
2022-07-28 11:05:34 -0300

    Exemplo do tipo string + concatenacao

---
+++ Tipo de dados e operadores logicos/String_Concatenação/Script.js
@@ -0,0 +1,7 @@
+Texto5 = 'Texto6'
+console.log('Texto 1' + ' mais algo')
+console.log(typeof 'Texto 1')
+console.log("Texto 2 \n Linha 2")
+console.log(`Texto 3`)
+//Adicionar variavel no meio do texto - Template literals
+console.log(`Texto 4 + ${Texto5}`)
